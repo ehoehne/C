b165a958105971fda478c13f5824ec72f0e34f84
Tony Rodriguez <85913666+TonyRodriguez2001@users.noreply.github.com>
2023-09-14 22:39:23 -0500

    Update list_test.c

--- list_test.c
+++ list_test.c
@@ -3,10 +3,54 @@
 
 int main(void){
     list* linkedList = create_list();
-    char anItem = 'a';
-    char item1 = 'b';
-    linkedList != NULL ? puts("Exists") : puts("Does Not Exist"); 
-    add_to_list(linkedList, &anItem);
-    add_to_list(linkedList, &item1);   
+    char *testStrings[] = {
+        "apple",
+        "banana",
+        "cherry",
+        "date",
+        "elderberry",
+        "fig",
+        "grape",
+        "honeydew",
+        "kiwi",
+        "lemon"
+    };
+
+    int numStrings = sizeof(testStrings) / sizeof(testStrings[0]);
+
+    printf("Creating the list... State of the list: ");
+    linkedList != NULL ? puts("The list exists\n") : puts("The list does not exist\n"); 
+
+    puts("Loading the list with the following strings:");
+    for(int i = 0; i < numStrings; i++){
+        if(i == numStrings - 1){
+            printf(" %s", testStrings[i]);
+        }
+        else{
+            printf(" %s,", testStrings[i]);
+        }
+        add_to_list(linkedList, testStrings[i]);
+    }
+
+    puts("\n");
+    puts("Printing list: ");
     print_list(linkedList);
+    puts("");
+
+    puts("Removing first 3 items of the list:");
+    for(int i = 0; i < 3; i++){
+        printf("Removing from list: %s\n", remove_from_list(linkedList));
+    }
+
+    puts("\nCurrent list: ");
+    print_list(linkedList);
+
+    puts("\n Flushing list...");
+    flush_list(linkedList);
+    linkedList != NULL ? puts("State of list after flush: The list exists\n") : puts("State of list after flush: The list does not exist\n"); 
+
+    puts("\nFreeing list...");
+    free_list(linkedList);
+    linkedList != NULL ? puts("The list still exists\n") : puts("The list no longer exists\n"); 
+
 }
